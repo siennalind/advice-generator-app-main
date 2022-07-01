936a7a93ec2ac7d5e1a9e0b01f52758dee691dc6
siennalind <sienna.lebeaulind@gmail.com>
2022-07-01 10:50:01 +0100

    init commit

---
+++ script.js
@@ -0,0 +1,4 @@
+const URL = 'https://api.adviceslip.com/advice';
+const randomAdviceButton = document.getElementById('randomButton');
+const adviceParagraph = document.getElementById('advice');
+const adviceId = document.getElementById('id');
