778f7bed8050cd5cff6596b0ee8790b990d7f721
siennalind <sienna.lebeaulind@gmail.com>
2022-08-06 15:31:25 +0100

    added api calls

--- script.js
+++ script.js
@@ -1,4 +1,18 @@
 const URL = 'https://api.adviceslip.com/advice';
-const randomAdviceButton = document.getElementById('randomButton');
+const randomAdviceButton = document.getElementById('button');
 const adviceParagraph = document.getElementById('advice');
 const adviceId = document.getElementById('id');
+
+async function getQuote () {
+    let data = await fetch(URL)
+      .then(response => response.json());
+
+    adviceId.innerText = data.slip.id
+    adviceParagraph.innerText = data.slip.advice
+}
+
+const getNewQuote = () => {
+    console.log('You clicked me!')
+}
+
+randomAdviceButton.addEventListener("click", getQuote)
